8bb3aab8cd249e3f826480f29ea78960f88f9d52
sumit027244 <kumarsumit27244@gmail.com>
2022-04-08 01:00:46 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-var a;
-a=10
-console.log(a)
-console.log(typeof a)
-a="hello"
-console.log(a)
-console.log(typeof a)
-a=true
-console.log(a)
-console.log(typeof a)
-a=null
-console.log(a)
-console.log(typeof a)
+// var a;
+// a=10
+// console.log(a)
+// console.log(typeof a)
+// a="hello"
+// console.log(a)
+// console.log(typeof a)
+// a=true
+// console.log(a)
+// console.log(typeof a)
+// a=null
+// console.log(a)
+// console.log(typeof a)
+
+var num=10
+console.log(num)
+var float=2.4
+console.log(float)
+
+var t=true
+var f=false
+console.log(t)
+console.log(f)
+
+// string
+// "",'',``
+// double quotes,single quotes,backticks
+var str=`abcd`
+console.log(str)
+str="how r u ?"
+console.log(str)
