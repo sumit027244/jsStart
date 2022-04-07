58d08de588c3d9ca8d478e475a76b01ed4870416
sumit027244 <kumarsumit27244@gmail.com>
2022-04-08 00:52:50 +0530

    js started

---
+++ index.js
@@ -0,0 +1,13 @@
+var a;
+a=10
+console.log(a)
+console.log(typeof a)
+a="hello"
+console.log(a)
+console.log(typeof a)
+a=true
+console.log(a)
+console.log(typeof a)
+a=null
+console.log(a)
+console.log(typeof a)
